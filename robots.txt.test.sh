668e4904dd165d40bdcddc013aab9a7883b9e124
Douglas Berdeaux <weaknetlabs@gmail.com>
2018-06-06 20:56:15 -0400

    Create robots.txt.test.sh

---
+++ robots.txt.test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Pass to me the URL:
+# e.g.:
+# ./robots.txt.test.sh http://10.10.0.22
+#
+wget $1/robots.txt
+for url in $(for uri in $(awk '{print $2}' robots.txt |egrep '^/'); 
+ do 
+  echo $1$uri; 
+ done); 
+ do curl -o /dev/null  --silent --head --write-out '%{url_effective}:%{http_code}\n' $url; 
+done
