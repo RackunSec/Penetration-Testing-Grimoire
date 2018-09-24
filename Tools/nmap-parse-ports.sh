30a5f200e75eb13e2a3a2f47617f9ad18025dfab
Douglas Berdeaux <weaknetlabs@gmail.com>
2018-09-24 12:07:12 -0400

    Added usage function. derp.

--- Tools/nmap-parse-ports.sh
+++ Tools/nmap-parse-ports.sh
@@ -2,11 +2,17 @@
 # NMAP output to CSV for ports script
 # Douglas Berdeaux
 IFS=$'\n'
+usage () {
+ echo "Usage: ./nmap-parse-output.txt (filename of NMAP init file)"
+ echo "  See Tools.md in repo for more information."
+}
 if [ $1 ]; then
  for line in $(cat $1);
   do
    output=$output$(echo $line | sed -r -e 's/\/.*//')",";
  done;
+ output=${output::-1} # Bash 4.2+ - Remove trailing comma/byte
+ echo "$output" # add a newline.
+else
+ usage
 fi
-output=${output::-1} # Bash 4.2+ - Remove trailing comma/byte
-echo "$output" # add a newline.
