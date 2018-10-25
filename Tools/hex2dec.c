1aaf23ffccdc758db3953d7e3c5665f0fb8f06d5
Douglas Berdeaux <weaknetlabs@gmail.com>
2018-10-25 10:45:32 -0400

    Added hex2Dec C tool

---
+++ Tools/hex2dec.c
@@ -0,0 +1,16 @@
+/*
+ Hex2Dec conversion tool.
+ 2018, Douglas Berdeaux, WeakNet Labs
+ Provide hex as "0x0..0xfff" format and get decimal value.
+*/
+#include<stdio.h>
+#include<stdlib.h> // str 2 ul
+int main(int *argc,char ** argv){
+ if(argc==2){
+  printf("dec: %d\n",strtoul(argv[1],0,16));
+ }else{
+  printf("\nhex2dec: WeakNetLabs\n[!] You must provide input as a hexadecimal value.\n[?] e.g: './hex2dec 0xaf3'\n\n");
+   return 1;
+ }
+ return 0;
+}
