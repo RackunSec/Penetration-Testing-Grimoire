638a9aed35d6041e77629dd133e4158ebc45d77e
Douglas Berdeaux <weaknetlabs@gmail.com>
2018-11-18 00:02:09 -0500

    Added feedback as Modulo (1000)

--- Tools/opensslDecrypt.sh
+++ Tools/opensslDecrypt.sh
@@ -8,25 +8,31 @@ cipher=$3
 printf "\nOpenSSLDecrypt Brute Force Tool.\n\n[*] Destroying and Creating temporary directory, ./opensslDecryptOutput\n"
 rm -rf opensslDecryptOutput
 mkdir opensslDecryptOutput
+count=0
 for passwd in $(cat $wordlist)
  do 
+  let "count++"
   passwd=$(echo $passwd | sed 's/[^0-9A-Za-z]//g') # remove all but alpha/num
- if [[ $passwd != "" ]]
-  then
-   result=$(openssl enc -d -$cipher -salt -in $encryptedFile -out opensslDecryptOutput/${passwd}.decrypted -k $passwd 2>/dev/stdout| head -n 1)
-   if [[ $result != 'bad decrypt' ]];
-    then
-     stringSearchPassword=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'password' |wc -l)
-     stringSearchUser=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'user'|wc -l)
-     stringSearchDrupal=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'drupal'|wc -l)
-     stringSearchFlag=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'flag'|wc -l)
-     stringSearchRoot=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'root'|wc -l)
-     if [[ $stringSearchDrupal -gt 0 || $stringSearchUser -gt 0 || $stringSearchDrupal -gt 0 || $stringSearchFlag -gt 0 || $stringSearchRoot -gt 0 ]]
-      then
-       printf "[!] Possible Passwd Found !!\nPASSWD: $passwd\n\n"
-      else
-       rm opensslDecryptOutput/${passwd}.decrypted # remove it.
-     fi
-   fi
- fi
+  if [[ $passwd != "" ]]
+   then
+    result=$(openssl enc -d -$cipher -salt -in $encryptedFile -out opensslDecryptOutput/${passwd}.decrypted -k $passwd 2>/dev/stdout| head -n 1)
+    if [[ $result != 'bad decrypt' ]];
+     then
+      stringSearchPassword=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'password' |wc -l)
+      stringSearchUser=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'user'|wc -l)
+      stringSearchDrupal=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'drupal'|wc -l)
+      stringSearchFlag=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'flag'|wc -l)
+      stringSearchRoot=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'root'|wc -l)
+      if [[ $stringSearchDrupal -gt 0 || $stringSearchUser -gt 0 || $stringSearchDrupal -gt 0 || $stringSearchFlag -gt 0 || $stringSearchRoot -gt 0 ]]
+       then
+        printf "[!] Possible Passwd Found !!\nPASSWD: $passwd\n\n"
+       else
+        rm opensslDecryptOutput/${passwd}.decrypted # remove it.
+      fi
+    fi
+  fi
+  if [[ $((count % 1000)) -eq 0 ]]
+   then
+    printf "[*] $count Passwords tried.\n"
+  fi
 done
