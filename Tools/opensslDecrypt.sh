8c64f93ac3564eb575583c6b5a8af96a35799d0d
Douglas Berdeaux <weaknetlabs@gmail.com>
2018-11-23 14:48:56 -0500

    Updated UX, Added File Type
    It just worked in a production CTF

--- Tools/opensslDecrypt.sh
+++ Tools/opensslDecrypt.sh
@@ -2,31 +2,29 @@
 # OpenSSL .enc file decryption cracking script
 # 2018 WeakNet Labs, Douglas Berdeaux, weaknetlabs@gmail.com
 # ./opensslDecrypt.sh (WORDLIST) (FILE TO CRACK) (CIPHER)
-wordlist=$1
+wordlist=$1 # I am renaming these for readability of the code
 encryptedFile=$2
 cipher=$3
-printf "\nOpenSSLDecrypt Brute Force Tool.\n\n[*] Destroying and Creating temporary directory, ./opensslDecryptOutput\n"
+printf "\nOpenSSLDecrypt Brute Force Tool.\n\n[*] Destroying and Creating temporary directory, ./opensslDecryptOutput\n\n"
 rm -rf opensslDecryptOutput
 mkdir opensslDecryptOutput
-count=0
-#for passwd in $(cat $wordlist)
-while read -r passwd
+count=0 # This token is used for UX feedback
+while read -r passwd # read the file in by line rather than all into memory causing a kill
  do
   let "count++"
   passwd=$(echo $passwd | sed 's/[^0-9A-Za-z]//g') # remove all but alpha/num
   if [[ $passwd != "" ]] # we need an actual password to try
    then # Get the decrypted version:
     result=$(openssl enc -$cipher -d -a -in $encryptedFile -out opensslDecryptOutput/${passwd}.decrypted -d -pass pass:"$passwd" 2>/dev/stdout| head -n 1)
-    # openssl enc -d -aes128 -salt -in drupal.txt -out opensslDecryptOutput/drupal.txt.decrypt -k friends | head -n 1
     if [[ $result != 'bad decrypt' ]]; # It wasn't a failure according to openSSL
      then
       if [[ -f "opensslDecryptOutput/${passwd}.decrypted" ]] # does the file exist?
        then
-	#printf "[*] Trying: $passwd\n"; # DEBUG
         resultTest=$(file opensslDecryptOutput/${passwd}.decrypted | grep ASCII | wc -l)
         if [[ $resultTest -eq 1 ]]
          then
-          printf "\n\n[!] Possible Passwd Found !!\nPASSWD: $passwd\n\n"
+          printf "\n[!] Possible Passwd Found !!\n[!] PASSWD: $passwd\n[*] File Type: $(file opensslDecryptOutput/${passwd}.decrypted)\n\n"
+          exit
         fi
       else
        printf "[!] Could not create a decrypted file. Something went wrong with the input.\n[*] Error: $result\n\n"
@@ -34,7 +32,7 @@ while read -r passwd
       fi
      fi
     if [[ $result == 'bad magic number' ]]; # the file is not an .enc file.
-     then 
+     then
       printf "[!] Not an encrypted file. Is this an encoded encrypted file?\n";
       exit 1 # EXIT hard here
     fi
@@ -42,6 +40,6 @@ while read -r passwd
   fi
   if [[ $((count % 1000)) -eq 0 ]]
    then
-    printf "[*] $count Passwords tried. ($passwd)\n"
+    printf "[*] $count Passwords tried. ($passwd:$result)\n"
   fi
 done < $wordlist
