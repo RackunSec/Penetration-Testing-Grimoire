775c76419a4e62c22ebeb664ca9bb0bf9e849167
Douglas Berdeaux <weaknetlabs@gmail.com>
2018-11-23 11:50:47 -0500

    Updated for more passive/graceful failures

--- Tools/opensslDecrypt.sh
+++ Tools/opensslDecrypt.sh
@@ -13,23 +13,36 @@ for passwd in $(cat $wordlist)
  do 
   let "count++"
   passwd=$(echo $passwd | sed 's/[^0-9A-Za-z]//g') # remove all but alpha/num
-  if [[ $passwd != "" ]]
-   then
+  if [[ $passwd != "" ]] # we need an actual password to try
+   then # Get the decrypted version:
     result=$(openssl enc -d -$cipher -salt -in $encryptedFile -out opensslDecryptOutput/${passwd}.decrypted -k $passwd 2>/dev/stdout| head -n 1)
-    if [[ $result != 'bad decrypt' ]];
+    #printf "DBUG: $result\n";
+    # openssl enc -d -aes128 -salt -in drupal.txt -out opensslDecryptOutput/drupal.txt.decrypt -k friends | head -n 1
+    if [[ $result != 'bad decrypt' ]]; # It wasn't a failure according to openSSL
      then
-      stringSearchPassword=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'password' |wc -l)
-      stringSearchUser=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'user'|wc -l)
-      stringSearchDrupal=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'drupal'|wc -l)
-      stringSearchFlag=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'flag'|wc -l)
-      stringSearchRoot=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'root'|wc -l)
-      if [[ $stringSearchDrupal -gt 0 || $stringSearchUser -gt 0 || $stringSearchDrupal -gt 0 || $stringSearchFlag -gt 0 || $stringSearchRoot -gt 0 ]]
+      printf "[!] Possible Passphrase found: $passwd\n";
+      if [[ -f "opensslDecryptOutput/${passwd}.decrypted" ]] # does the file exist?
        then
-        printf "[!] Possible Passwd Found !!\nPASSWD: $passwd\n\n"
-       else
-        rm opensslDecryptOutput/${passwd}.decrypted # remove it.
+        stringSearchPassword=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'password' |wc -l)
+        stringSearchUser=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'user'|wc -l)
+        stringSearchDrupal=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'drupal'|wc -l)
+        stringSearchFlag=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'flag'|wc -l)
+        stringSearchRoot=$(strings opensslDecryptOutput/${passwd}.decrypted | egrep -i 'root'|wc -l)
+        if [[ $stringSearchDrupal -gt 0 || $stringSearchUser -gt 0 || $stringSearchDrupal -gt 0 || $stringSearchFlag -gt 0 || $stringSearchRoot -gt 0 ]]
+         then
+          printf "[!] Possible Passwd Found !!\nPASSWD: $passwd\n\n"
+        fi
+      else
+       printf "[!] Could not create a decrypted file. Something went wrong with the input.\n"
+       exit 1;
       fi
+     fi
+    if [[ $result == 'bad magic number' ]]; # the file is not an .enc file.
+     then 
+      printf "[!] Not an encrypted file. Is this an encoded encrypted file?\n";
+      exit 1 # EXIT hard here
     fi
+    rm opensslDecryptOutput/${passwd}.decrypted # remove it.
   fi
   if [[ $((count % 1000)) -eq 0 ]]
    then
