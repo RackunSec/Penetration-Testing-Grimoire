213511e8785abbecaf1d8d8ff5fe47b3ba7c4e42
Douglas Berdeaux <weaknetlabs@gmail.com>
2018-11-26 15:50:39 -0500

    Create bfhttp.sh

---
+++ Tools/bfhttp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# 2018 WeakNetLabs.com Douglas Berdeaux
+#
+if [[ "$2" == "" ]] # nothing passed
+ then
+  printf "Usage: ./bfhttp.sh (URL) (/PATH/TO/WORDLIST)
+  exit 1
+fi
+
+url=$1
+wordlist=$2
+count=0
+while read word
+ do
+  let "count++"
+  if ! (($count % 100))
+   then
+    printf "[*] $count Files Tried.\n"
+  fi
+  resp=$(curl -I -s $url$word | head -n 1)
+  if [[ ${resp} != "" ]] && [[ ${resp} != *"404"* ]]
+   then
+    printf "[!] $word: $resp\n"
+  fi
+done < $wordlist
